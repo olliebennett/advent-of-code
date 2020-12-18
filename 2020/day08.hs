47132e0170cb1c8fe2f82f21a123529d3a5168ea
Ollie Bennett <olliebennett@gmail.com>
2020-12-18 08:20:38 +0000

    D08:2 Refactor instruction handling

--- 2020/day08.hs
+++ 2020/day08.hs
@@ -3,22 +3,35 @@ import Data.List
 
 input = readDayInputLines 8
 
-accumulatorDelta :: (String, Int) -> Int
+data Instruction = Instruction { op :: String, idx :: Int } deriving (Eq, Show)
+
+isAcc :: Instruction -> Bool
+isAcc instruction = do
+  op instruction == "acc"
+
+isJmp :: Instruction -> Bool
+isJmp instruction = do
+  op instruction == "jmp"
+
+isNop :: Instruction -> Bool
+isNop instruction = do
+  op instruction == "nop"
+
+accumulatorDelta :: Instruction -> Int
 accumulatorDelta instruction = do
-  if fst instruction == "acc" then
-    snd instruction
+  if isAcc instruction then
+    idx instruction
   else
     0 -- only 'acc' has any effect on the accumulator value
 
-indexDelta :: (String, Int) -> Int
+indexDelta :: Instruction -> Int
 indexDelta instruction = do
-  if fst instruction == "jmp" then
-    -- 'jmp' has custom effect on the index value
-    snd instruction
+  if isJmp instruction then
+    idx instruction
   else
     1 -- other instructions simply increment by 1
 
-processInstructions :: Int -> [(String, Int)] -> [Int] -> Int -> Int
+processInstructions :: Int -> [Instruction] -> [Int] -> Int -> Int
 processInstructions idx instructions visited_indexes accumulator = do
   let instruction = instructions !! idx
   let next_idx = idx + indexDelta instruction
@@ -32,36 +45,27 @@ processInstructions idx instructions visited_indexes accumulator = do
     -- Recursively iterate
     processInstructions next_idx instructions updated_visited_indexes updated_accumulator
 
-parseInstruction :: String -> (String, Int)
+parseInstruction :: String -> Instruction
 parseInstruction line = do
   let x = splitStr " " line
-  let op = head x
   let val = x !! 1
   let num = str2int (tail val)
   if head val == '+' then
-    (op, num)
+    Instruction { op = head x, idx = num }
   else
-    (op, (-1) * num)
+    Instruction { op = head x, idx = (-1) * num }
 
 main = do
   putStrLn "AoC 2020 - Day 8"
 
   putStrLn "-- Part 1 Tests"
 
-  let test_input = ["nop +0"
-                  , "acc +1"
-                  , "jmp +4"
-                  , "acc +3"
-                  , "jmp -3"
-                  , "acc -99"
-                  , "acc +1"
-                  , "jmp -4"
-                  , "acc +6"]
+  let test_input = ["nop +0", "acc +1", "jmp +4", "acc +3", "jmp -3", "acc -99", "acc +1", "jmp -4", "acc +6"]
   let test_instructions = map parseInstruction test_input
 
-  test "parse 'nop +0'" ("nop", 0) (parseInstruction "nop +0")
-  test "parse 'acc +4'" ("jmp", 4) (parseInstruction "jmp +4")
-  test "parse 'jmp -6'" ("jmp", -6) (parseInstruction "jmp -6")
+  test "parse 'nop +0'" Instruction { op = "nop", idx = 0 } (parseInstruction "nop +0")
+  test "parse 'acc +4'" Instruction { op = "jmp", idx = 4 } (parseInstruction "jmp +4")
+  test "parse 'jmp -6'" Instruction { op = "jmp", idx = -6 } (parseInstruction "jmp -6")
 
   test "process" 5 (processInstructions 0 test_instructions [] 0)
 
@@ -69,5 +73,7 @@ main = do
   let instructions = map parseInstruction input
   let visited_indexes = []
   let accumulator = 0
-  putStrLn $ "Accumulator val: " ++ show (processInstructions 0 instructions visited_indexes accumulator)
+  let result = processInstructions 0 instructions visited_indexes accumulator
+  putStrLn $ "Accumulator before crash val: " ++ show result
 
+  putStrLn "-- Part 2 Test"
